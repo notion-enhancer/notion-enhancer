d8351aa49eae5f2db637ba0c2fb58d0f5ff653bf
dragonwocky <thedragonring.bod@gmail.com>
2020-08-02 23:14:13 +1000

    mod info boxes

--- repo/core/mod.js
+++ repo/core/mod.js
@@ -18,10 +18,10 @@ const defaults = {
 
 module.exports = {
   id: '0f0bf8b6-eae6-4273-b307-8fc43f2ee082',
-  type: 'core',
+  tags: ['core', 'extension'],
   name: 'notion-enhancer core',
-  desc:
-    'the modloader itself, including: the CLI, the menu, and enabling/disabling/insertion/updating of mods.',
+  desc: `the **modloader** itself, _including_: the [CLI](https://github.com), the menu, and ~~enabling/disabling/insertion/updating of~~ mods.
+    ![](https://preview.redd.it/vtiw9ulqlt951.png?width=1368&format=png&auto=webp&s=733d8b27ec62151c7858b4eca463f809ead6395a)`,
   version: require('../../package.json').version,
   author: 'dragonwocky',
   thumb:
