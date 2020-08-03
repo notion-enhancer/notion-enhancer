158ce182df1cd97e09c62d50a1b99dc664417426
dragonwocky <thedragonring.bod@gmail.com>
2020-08-04 00:31:27 +1000

    mod toggling, first extension: bracketed links

---
+++ repo/bracketed-links/mod.js
@@ -0,0 +1,16 @@
+/*
+ * notion-enhancer
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com>
+ * (https://dragonwocky.me/) under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: '60e14feb-a81d-4ffb-9b12-7585d346bad8',
+  tags: ['extension'],
+  name: 'bracketed links',
+  desc: 'render links surrounded with [brackets] instead of __underlined__.',
+  version: '0.0.1',
+  author: 'arecsu',
+};
--- repo/core/mod.js
+++ repo/core/mod.js
@@ -20,12 +20,10 @@ module.exports = {
   id: '0f0bf8b6-eae6-4273-b307-8fc43f2ee082',
   tags: ['core', 'extension'],
   name: 'notion-enhancer core',
-  desc: `the **modloader** itself, _including_: the [CLI](https://github.com), the menu, and ~~enabling/disabling/insertion/updating of~~ mods.
+  desc: `the **modloader** itself, _including_: the [CLI](https://github.com), the \`menu\`, and ~~enabling/disabling/insertion/updating of~~ mods.
     ![](https://preview.redd.it/vtiw9ulqlt951.png?width=1368&format=png&auto=webp&s=733d8b27ec62151c7858b4eca463f809ead6395a)`,
   version: require('../../package.json').version,
   author: 'dragonwocky',
-  thumb:
-    'https://camo.githubusercontent.com/5c5bca9e987d986b8cc7e51066f90c6f8a84af08/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3733313634373938343332333931393933332f3733313732373235393239353032333132342f494d475f323137302e6a7067',
   options: [],
   hacks: {
     'main/main.js': require('./tray.js')(defaults),
