7f779e35e501e9b075f6c929a781ca0761f050fb
dragonwocky <thedragonring.bod@gmail.com>
2020-08-05 21:48:11 +1000

    working menu search

--- repo/custom-inserts/mod.js
+++ repo/custom-inserts/mod.js
@@ -10,7 +10,7 @@ module.exports = {
   id: 'b4b0aced-2059-43bf-8d1d-ccd757ee5ebb',
   tags: ['extension'],
   name: 'custom inserts',
-  desc: 'directly link files for small client-side tweaks.',
+  desc: 'link files for small client-side tweaks.',
   version: '0.0.2',
   author: 'dragonwocky',
   options: [
@@ -27,4 +27,31 @@ module.exports = {
       extensions: ['js'],
     },
   ],
+  hacks: {
+    'renderer/preload.js'(store, __exports) {
+      const fs = require('fs-extra');
+      document.addEventListener('readystatechange', (event) => {
+        if (document.readyState !== 'complete') return false;
+        if (store().css) {
+          try {
+            const style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = fs.readFileSync(store().css);
+            document.querySelector('head').appendChild(style);
+          } catch (err) {
+            console.warn('<custom-inserts> invalid css file... unsetting.');
+            store().css = '';
+          }
+        }
+        if (store().js) {
+          try {
+            require(store().js);
+          } catch (err) {
+            console.warn('<custom-inserts> invalid js file... unsetting.');
+            store().js = '';
+          }
+        }
+      });
+    },
+  },
 };
