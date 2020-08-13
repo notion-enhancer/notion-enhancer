64358e5576120615e64245e13814398040cb0dec
dragonwocky <thedragonring.bod@gmail.com>
2020-08-13 23:24:45 +1000

    purple gameish theme... need to link to reddit account

---
+++ mods/gameish/mod.js
@@ -0,0 +1,17 @@
+/*
+ * gameish
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * (c) 2020 alizarin
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: 'ad923617-e76e-408e-9f23-490738a3223f',
+  tags: ['theme', 'dark'],
+  name: 'gameish',
+  desc: 'a purple, "gamer-styled" theme with a blocky-font.',
+  version: '0.0.3',
+  author: 'alizarin',
+};
