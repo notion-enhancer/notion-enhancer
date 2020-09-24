9d342fa99ee2e72f9438f811f9521b71efb91ed1
blacksuan19 <i@blacksuan19.me>
2020-09-24 13:46:27 +0800

    mods: add material ocean theme
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

---
+++ repo/material-ocean/mod.js
@@ -0,0 +1,16 @@
+/*
+ * Material Ocean
+ * (c) 2020 Abubakar Yagoub <i@blacksuan19.me> (https://blacksuan19.tk)
+ * under GNU's GPL3 license
+ */
+
+"use strict";
+
+module.exports = {
+  id: "69e7ccb2-4aef-484c-876d-3de1b433d2b9",
+  tags: ["theme", "dark"],
+  name: "Material Ocean",
+  desc: "a Material theme with oceanic colors",
+  version: "0.1",
+  author: "blacksuan19",
+};
