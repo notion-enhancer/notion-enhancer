c3a7bda6250940596e2a1f529a10f7420335c294
Mimi Shahzad <mimi.shahzad@virginpulse.com>
2020-10-06 17:32:23 -0400

    Dracula Theme
    A theme created based on the official Dracula Theme palette found https://draculatheme.com/contribute/. I do not own Dracula, but thought it would be nice to share this with others.
    
    Added a mod.js file as well as a folder for the theme files
    
    The theme works outside of the developer build.
    
    Misunderstood what you meant by license field.
    
    Just corrected it.

---
+++ mods/dracula/mod.js
@@ -0,0 +1,22 @@
+/*
+ * dracula
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * (c) 2020 u/mimi-shahzad
+ * (c) 2020 Dracula Theme
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: '033bff54-50ba-4cec-bdc0-b2ca7e307086',
+  tags: ['theme', 'dark'],
+  name: 'dracula',
+  desc: 'a theme based on the popular Dracula color palette originally by Zeno Rocha and friends. ',
+  version: '0.1',
+  author: {
+    name: 'mimi-shahzad',
+    link: 'https://github.com/mimi-shahzad',
+    avatar: 'https://secure.gravatar.com/avatar/86c98dec3a06245dbf2291deb44441f5',
+  }
+};
