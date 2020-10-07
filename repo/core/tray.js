c0d7d5170b00225c8f7d8f21f91bf65c4671daa4
dragonwocky <thedragonring.bod@gmail.com>
2020-10-07 23:58:22 +1100

    replace dragarea with titlebar in tabbable mode

--- repo/core/tray.js
+++ repo/core/tray.js
@@ -28,15 +28,15 @@ module.exports = (store, __exports) => {
           })
     );
 
-    electron.ipcMain.on('enhancer:set-theme-vars', (event, arg) => {
+    electron.ipcMain.on('enhancer:set-menu-theme', (event, arg) => {
       if (!enhancer_menu) return;
-      enhancer_menu.webContents.send('enhancer:set-theme-vars', arg);
+      enhancer_menu.webContents.send('enhancer:set-menu-theme', arg);
     });
-    electron.ipcMain.on('enhancer:get-theme-vars', (event, arg) => {
+    electron.ipcMain.on('enhancer:get-menu-theme', (event, arg) => {
       electron.webContents
         .getAllWebContents()
         .forEach((webContents) =>
-          webContents.send('enhancer:get-theme-vars', arg)
+          webContents.send('enhancer:get-menu-theme', arg)
         );
     });
 
