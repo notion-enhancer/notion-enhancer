bb18f2a1e69ddcc191befd81dddf90e3e6b9a76d
dragonwocky <thedragonring.bod@gmail.com>
2020-10-09 20:56:21 +1100

    bugfixes so dracula will actually load

--- repo/dracula-theme/mod.js
+++ repo/dracula-theme/mod.js
@@ -1,6 +1,5 @@
 /*
- * dracula
- * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * dracula-theme
  * (c) 2020 u/mimi-shahzad
  * (c) 2020 Dracula Theme
  * under the MIT license
@@ -12,11 +11,8 @@ module.exports = {
   id: '033bff54-50ba-4cec-bdc0-b2ca7e307086',
   tags: ['theme', 'dark'],
   name: 'dracula',
-  desc: 'a theme based on the popular Dracula color palette originally by Zeno Rocha and friends. ',
-  version: '0.1',
-  author: {
-    name: 'mimi-shahzad',
-    link: 'https://github.com/mimi-shahzad',
-    avatar: 'https://secure.gravatar.com/avatar/86c98dec3a06245dbf2291deb44441f5',
-  }
+  desc:
+    'a theme based on the popular dracula color palette originally by zeno rocha and friends. ',
+  version: '0.1.0',
+  author: 'mimi-shahzad',
 };
