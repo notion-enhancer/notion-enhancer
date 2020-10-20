2d6141115d86ca79f17d1f6153d208bffd54b4b1
CloudHill <rh.cloudhill@gmail.com>
2020-10-20 23:43:22 +0700

    Hide button on initialization if top distance is set

--- mods/scroll-to-top/mod.js
+++ mods/scroll-to-top/mod.js
@@ -56,6 +56,9 @@ module.exports = {
                     $container.append($scroll);
                     $container.append($help);
                     
+                    if (store().top > 0)
+                        $scroll.classList.add('hidden');
+
                     $scroll.addEventListener('click', () => {
                         document
                         .querySelector('.notion-frame > .notion-scroller')
