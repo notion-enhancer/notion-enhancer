6b6626c966134b011bb7e5e1e50a70345ec87eb3
Ryo Hilmawan <54142180+CloudHill@users.noreply.github.com>
2020-11-23 14:00:59 +0700

    upload outliner extension

---
+++ mods/outliner/mod.js
@@ -0,0 +1,23 @@
+/*
+ * outliner
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * (c) 2020 CloudHill
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: '87e077cc-5402-451c-ac70-27cc4ae65546',
+  tags: ['extension', 'panel'],
+  name: 'outliner',
+  desc: 'table of contents.',
+  version: '1.0.0',
+  author: 'CloudHill',
+  panel: {
+    html: "panel.html",
+    name: "Outline",
+    icon: "icon.svg",
+    js: "panel.js",
+  }
+};
