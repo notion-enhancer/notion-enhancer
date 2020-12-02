4a9d6477b7552f34e869d6decbf7ba0a9eb61d0c
Ryo Hilmawan <54142180+CloudHill@users.noreply.github.com>
2020-12-02 19:05:33 +0700

    New extensions: side panel and outliner (#284)
    * upload side panel core
    
    * upload outliner extension
    
    * add onLoad, onSwitch, and onResize functions to panel extensions
    
    * Update panel.js
    
    * Add onLock and onUnlock functions to panel extensions
    
    * Reordered side panel content loading
    
    * change panel switcher font color
    
    * fix loading last opened panel
    
    * use svg icon files instead of defining them inline
    
    * outliner: add full height option

---
+++ mods/outliner/mod.js
@@ -0,0 +1,34 @@
+/*
+ * outliner
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * (c) 2020 CloudHill
+ * under the MIT license
+ */
+
+'use strict';
+
+const store = require("../../pkg/store");
+
+module.exports = {
+  id: '87e077cc-5402-451c-ac70-27cc4ae65546',
+  tags: ['extension', 'panel'],
+  name: 'outliner',
+  desc: 'table of contents.',
+  version: '1.0.0',
+  author: 'CloudHill',
+  options: [
+    {
+      key: 'fullHeight',
+      label: 'full height',
+      type: 'toggle',
+      value: false
+    }
+  ],
+  panel: {
+    html: "panel.html",
+    name: "Outline",
+    icon: "icon.svg",
+    js: "panel.js",
+    fullHeight: store('87e077cc-5402-451c-ac70-27cc4ae65546').fullHeight
+  }
+};
