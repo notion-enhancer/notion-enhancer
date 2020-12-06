dc17590b84e2b2f09b494e3f6752bc7373bed5de
mugiwarafx <sangalesbenes@uoc.edu>
2020-12-06 20:36:27 +0100

    fix: folder theme path

--- mods/pinky-boom-theme/mod.js
+++ mods/pinky-boom-theme/mod.js
@@ -9,8 +9,8 @@
 module.exports = {
     id: "fbef391c-58ff-4938-bd45-b85ae0435e4e",
     name: "Pinky Boom Theme",
-    tags: ['theme', 'pink'],
-    desc: "Pinky everywhere and black cats",
+    tags: ['theme', 'pink', 'dark', 'light'],
+    desc: "Pinkify your dark/light default template!",
     version: "0.0.1",
     author: "mugiwarafx",
 };
