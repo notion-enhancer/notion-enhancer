e12b3bac85e44c3a6eb648679b632ed6c1ba4e7f
mugiwarafx <58401248+mugiwarafx@users.noreply.github.com>
2020-12-13 18:18:13 +0100

    feat(theme): add pinky boom light theme (#348)
    * feat(theme): add pinky boom theme
    
    * fix: folder theme path
    
    * fix: proper var syntax
    
    * feat(theme): add pinky boom theme
    
    * feat(theme): add pinky boom light theme
    
    * add newline mod.js
    
    * add newline variables.css

---
+++ mods/pinky-boom-light/mod.js
@@ -0,0 +1,16 @@
+/*
+ * Pinky Booooom Theme!
+ * (c) 2020 mugiwarafx (https://github.com/mugiwarafx)
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+    id: "fbef391c-58ff-4938-bd45-b85ae0435e4e",
+    name: "Pinky Boom Theme",
+    tags: ['theme', 'light', 'pink'],
+    desc: "pinkify your light theme",
+    version: "1.0.0",
+    author: "mugiwarafx",
+};
