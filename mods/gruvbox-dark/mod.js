91dd6837bdf073212ac674c1529db716f3a0449c
Jordan Robinson <me@jordanrobinson.co.uk>
2021-01-23 12:31:17 +0000

    Adds gruvbox-dark theme based on morhetz gruvbox vim theme

---
+++ mods/gruvbox-dark/mod.js
@@ -0,0 +1,16 @@
+/*
+ * gruvbox dark
+ * (c) 2021 jordanrobinson <me@jordanrobinson.co.uk> (https://jordanrobinson.co.uk)
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: 'ad0f5c5c-8b62-4b20-8a54-929e663ea368',
+  tags: ['theme', 'dark'],
+  name: 'gruvbox dark',
+  desc: 'gruvbox dark, based on the vim theme from morhetz.',
+  version: '0.2.0',
+  author: 'jordanrobinson',
+};
