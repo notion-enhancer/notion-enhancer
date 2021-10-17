69c3034a8b5cb2c994f191caf0ec224f53fccc16
dragonwocky <thedragonring.bod@gmail.com>
2021-10-17 22:17:33 +1100

    rainbow indentation lines + block dragger bg fix

--- repo/indentation-lines/client.mjs
+++ repo/indentation-lines/client.mjs
@@ -8,7 +8,8 @@
 
 export default async function ({ web }, db) {
   let style = 'solid',
-    opacity = 1;
+    opacity = 1,
+    rainbow = false;
   switch (await db.get(['style'])) {
     case 'dashed':
       style = 'dashed';
@@ -19,25 +20,53 @@ export default async function ({ web }, db) {
     case 'soft':
       opacity = 0.25;
       break;
+    case 'rainbow':
+      opacity = 0.7;
+      rainbow = true;
+      break;
   }
+
+  let css = '';
+  const colors = ['red', 'pink', 'purple', 'blue', 'green', 'yellow'];
+  colors.push(...colors, ...colors, ...colors, 'gray');
 
   for (const listType of ['bulleted_list', 'numbered_list', 'to_do', 'toggle']) {
     if (!(await db.get([listType]))) continue;
-    document.head.append(web.html`<style>
+    css += `
       .notion-page-content .notion-${listType}-block > div > div:last-child::before {
-        border-left: 1px ${style};
+        border-left: 1px ${style} var(--indentation_lines--color, currentColor);
         opacity: ${opacity};
+      }`;
+
+    if (rainbow) {
+      for (let i = 0; i < colors.length; i++) {
+        css += `
+          .notion-page-content ${`.notion-${listType}-block `.repeat(i + 1)}
+            > div > div:last-child::before {
+            --indentation_lines--color: var(--theme--text_${colors[i]});
+          }`;
       }
-    </style>`);
+    }
   }
 
   if (db.get(['table_of_contents'])) {
-    document.head.append(web.html`<style>
+    css += `
       .notion-page-content .notion-table_of_contents-block > div > div > a > div
-      > div:not([style*='margin-left: 0px']) > div::before {
-        border-left: 1px ${style};
+        > div:not([style*='margin-left: 0px']) > div::before {
+        border-left: 1px ${style} var(--indentation_lines--color, currentColor);
         opacity: ${opacity};
+      }`;
+
+    if (rainbow) {
+      for (let i = 0; i < colors.length; i++) {
+        css += `
+          .notion-page-content ${`.notion-table_of_contents-block `.repeat(i + 1)}
+            > div > div > a > div > div:not([style*='margin-left: 0px']) > div::before {
+            --indentation_lines--color: var(--theme--text_${colors[i]});
+          }`;
       }
-    </style>`);
+    }
   }
+
+  document.head.append(web.html`<style>${css}</style>`);
 }
