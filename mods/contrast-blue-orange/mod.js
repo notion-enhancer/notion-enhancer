d44ab2f87de249de29d8105d7f41b8fd0420a3ba
GuYith <48597370+GuYith@users.noreply.github.com>
2021-10-19 20:26:05 +0800

    add a light theme contrast-blue-orange

---
+++ mods/contrast-blue-orange/mod.js
@@ -0,0 +1,16 @@
+/*
+ * contrast-blue-orange
+ * (c) 2021 GuYith (https://github.com/GuYith)
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+    id:'61dd3671-f8b2-4116-8f1c-1a169286fe55',
+    name: 'contrast blue orange',
+    tags: ['theme', 'light', 'bule'],
+    desc: 'A bright light theme using blue and orange',
+    version: '0.1.0',
+    author: 'GuYith',
+};
