616686b3c8c46c61769a81058c1d1d5dd6692aef
dragonwocky <thedragonring.bod@gmail.com>
2021-11-07 18:57:22 +1100

    move changelog

--- extension/init.js
+++ extension/init.js
@@ -13,22 +13,23 @@
     signedIn = localStorage['LRU:KeyValueStore2:current-user-id'];
 
   if (site || page || (whitelisted && signedIn)) {
-    import(chrome.runtime.getURL('api/_.mjs')).then(async (api) => {
-      const { fs, registry, web } = api;
-      for (const mod of await registry.list((mod) => registry.enabled(mod.id))) {
-        for (const sheet of mod.css?.client || []) {
-          web.loadStylesheet(`repo/${mod._dir}/${sheet}`);
-        }
-        for (let script of mod.js?.client || []) {
-          script = await import(fs.localPath(`repo/${mod._dir}/${script}`));
-          script.default(api, await registry.db(mod.id));
-        }
+    const api = await import(chrome.runtime.getURL('api/_.mjs')),
+      { fs, registry, web } = api;
+
+    for (const mod of await registry.list((mod) => registry.enabled(mod.id))) {
+      for (const sheet of mod.css?.client || []) {
+        web.loadStylesheet(`repo/${mod._dir}/${sheet}`);
       }
-      const errors = await registry.errors();
-      if (errors.length) {
-        console.log('[notion-enhancer] registry errors:');
-        console.table(errors);
+      for (let script of mod.js?.client || []) {
+        script = await import(fs.localPath(`repo/${mod._dir}/${script}`));
+        script.default(api, await registry.db(mod.id));
       }
-    });
+    }
+
+    const errors = await registry.errors();
+    if (errors.length) {
+      console.log('[notion-enhancer] registry errors:');
+      console.table(errors);
+    }
   }
 })();
