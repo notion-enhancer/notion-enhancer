3f5ff3642d9501659af959579d9caaba5519e956
dragonwocky <thedragonring.bod@gmail.com>
2021-12-24 23:36:46 +1100

    env jsdoc changes

--- extension/env/env.mjs
+++ extension/env/env.mjs
@@ -1,15 +1,12 @@
 /*
- * notion-enhancer core: api
+ * notion-enhancer: api
  * (c) 2021 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
  * (https://notion-enhancer.github.io/) under the MIT license
  */
 
 'use strict';
 
-/**
- * environment-specific methods and constants
- * @module notion-enhancer/api/env
- */
+/** environment-specific methods and constants */
 
 /**
  * the environment/platform name code is currently being executed in
--- extension/env/storage.mjs
+++ extension/env/storage.mjs
@@ -1,23 +1,20 @@
 /*
- * notion-enhancer core: api
+ * notion-enhancer: api
  * (c) 2021 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
  * (https://notion-enhancer.github.io/) under the MIT license
  */
 
 'use strict';
 
-/**
- * environment-specific data persistence
- * @module notion-enhancer/api/storage
- */
+/** environment-specific data persistence */
 
 const _queue = [],
   _onChangeListeners = [];
 
 /**
  * get persisted data
- * @param {array<string>} path - the path of keys to the value being fetched
- * @param {*} [fallback] - a default value if the path is not matched
+ * @param {string[]} path - the path of keys to the value being fetched
+ * @param {unknown=} fallback - a default value if the path is not matched
  * @returns {Promise} value ?? fallback
  */
 export const get = (path, fallback = undefined) => {
@@ -39,8 +36,8 @@ export const get = (path, fallback = undefined) => {
 
 /**
  * persist data
- * @param {array<string>} path - the path of keys to the value being set
- * @param {*} value - the data to save
+ * @param {string[]} path - the path of keys to the value being set
+ * @param {unknown} value - the data to save
  * @returns {Promise} resolves when data has been saved
  */
 export const set = (path, value) => {
@@ -80,9 +77,9 @@ export const set = (path, value) => {
 
 /**
  * create a wrapper for accessing a partition of the storage
- * @param {array<string>} namespace - the path of keys to prefix all storage requests with
- * @param {function} [get] - the storage get function to be wrapped
- * @param {function} [set] - the storage set function to be wrapped
+ * @param {string[]} namespace - the path of keys to prefix all storage requests with
+ * @param {function=} get - the storage get function to be wrapped
+ * @param {function=} set - the storage set function to be wrapped
  * @returns {object} an object with the wrapped get/set functions
  */
 export const db = (namespace, getFunc = get, setFunc = set) => {
@@ -114,6 +111,6 @@ export const removeChangeListener = (callback) => {
  * @callback onStorageChangeCallback
  * @param {object} event
  * @param {string} event.path- the path of keys to the changed value
- * @param {string} [event.new] - the new value being persisted to the store
- * @param {string} [event.old] - the previous value associated with the key
+ * @param {string=} event.new - the new value being persisted to the store
+ * @param {string=} event.old - the previous value associated with the key
  */
